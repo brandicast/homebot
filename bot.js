69a6a08da1d5fa1dfe07afabd286fb8687861ef4
brandicast <brandicast@gmail.com>
2024-08-06 11:03:44 +0800

    fix when lost member info

--- bot.js
+++ bot.js
@@ -4,16 +4,16 @@ const log4js = require("log4js");
 log4js.configure(config.log4js_set);
 const logger = log4js.getLogger(__filename.slice(__dirname.length + 1));
 
-let member_manager = require("./bot_member_manager.js") ;
-let vocab_db = require ("./vocab_manager.js") ;
+let member_manager = require("./bot_member_manager.js");
+let vocab_db = require("./vocab_manager.js");
 
 
 let linebot = require('linebot');
 
-let composer = require ("./line_message_formatter.js") ;
+let composer = require("./line_message_formatter.js");
 let message = require("./lib/line_message_template.js");
 
-var mqtt_agent  = require ('./mqtt_agent.js');
+var mqtt_agent = require('./mqtt_agent.js');
 
 let bot = linebot(config.linebot.configuration);
 
@@ -24,18 +24,18 @@ var MEMBERS = {};
   
 */
 bot.on('message', function (event) {
-    var response ; // get text json from template
+    var response; // get text json from template
     switch (event.message.type) {
         case "text": {
             var text = event.message.text;
             //logger.debug("TEXT = : " + text); 
             //logger.debug(JSON.stringify(event)); 
-            
+
             switch (text) {
                 case "(STATUS)": {
                     response = Object.assign({}, message.flex.common);
                     var carousel = Object.assign({}, message.flex.container.carousel);
-                    response.contents = carousel ;
+                    response.contents = carousel;
                     var bubbles = [];
                     bubbles[0] = Object.assign({}, message.flex.container.bubble);
                     bubbles[1] = Object.assign({}, message.flex.container.bubble);
@@ -50,151 +50,151 @@ bot.on('message', function (event) {
                     response = Object.assign({}, message.flex.common);
                     var bubble = Object.assign({}, message.flex.container.bubble);
                     bubble.body.contents[0].text = "I am bubble 1";
-                    response.contents = bubble ;
+                    response.contents = bubble;
                     logger.debug(JSON.stringify(response));
                     event.reply(response);
-                    break; 
+                    break;
                 }
-                case "(status)" : {
+                case "(status)": {
                     response = composer.getStatus();
                     event.reply(response);
                     break;
                 }
-                case "(raw)" :{
-                    response = composer.getRawData() ;
+                case "(raw)": {
+                    response = composer.getRawData();
                     event.reply(response);
                     break;
                 }
-                case "麵包 請幫忙開大門" :{
+                case "麵包 請幫忙開大門": {
 
-                    response =  Object.assign({}, message.text); 
+                    response = Object.assign({}, message.text);
                     response.text = config.linebot.try_to_open_front_door
 
                     var code = mqtt_agent.publish(config.mqtt.topic_for_pico, "OPEN")
-                    if (code == 0) 
+                    if (code == 0)
                         response.text = config.linebot.fail_to_open_front_door
                     event.reply(response);
                     break;
                 }
-                case "麵包教我單字":{
+                case "麵包教我單字": {
                     var word = vocab_db.randomBasicWord();
 
                     var bubble = Object.assign({}, message.flex2.container.bubble);
-                    bubble.header.contents[0].text= word[0] ;
+                    bubble.header.contents[0].text = word[0];
 
-                    delete bubble.hero ;   // No hero image for now 
+                    delete bubble.hero;   // No hero image for now 
 
                     target = word[1].chi;
                     //target = "[形容詞] 二的; [名詞] 二" ;
                     //target = "[副詞] 只,僅僅,才;不料;反而;可是,不過;要不是,若非 ; [連接詞] 可是,不過;要不是,若非 ";
                     //target = "哈哈"
-                                            
+
                     explain_array = String(target).split("[");  //  explain_array = String(target).split(/(\[)/g);  -> this returns delimiter as item
 
-                    content_array = [] ;
+                    content_array = [];
 
                     explain_array.forEach(element => {
-                        if (element.length > 0){
-                            item = {} ;
-                            item["type"] = "text" ;
-                            if (element.indexOf("]")>0)
-                                element = "[" + element ;
-                            item["text"] =  element ;
-                            item["size"] = "lg" ;
-                            item["align"] = "start" ;
-                            item["wrap"] =  true ;
-                            content_array.push (item) ;
+                        if (element.length > 0) {
+                            item = {};
+                            item["type"] = "text";
+                            if (element.indexOf("]") > 0)
+                                element = "[" + element;
+                            item["text"] = element;
+                            item["size"] = "lg";
+                            item["align"] = "start";
+                            item["wrap"] = true;
+                            content_array.push(item);
                         }
                     });
                     //bubble.body.contents[0].text = word[1].chi ;
-                    bubble.body.contents = content_array ;
+                    bubble.body.contents = content_array;
 
-                    if ("None" == word[1].link) 
-                        delete bubble.footer ;
-                    else 
-                        bubble.footer.contents[1].action.uri = config.eng.base_url + word[1].link ;
+                    if ("None" == word[1].link)
+                        delete bubble.footer;
+                    else
+                        bubble.footer.contents[1].action.uri = config.eng.base_url + word[1].link;
 
                     response = Object.assign({}, message.flex2.common);
-                    response.contents = bubble ;
+                    response.contents = bubble;
 
                     //logger.debug(JSON.stringify(response));
                     event.reply(response);
 
-                    break ;
+                    break;
                 }
-                case "麵包教我難一點的單字":{
+                case "麵包教我難一點的單字": {
                     var word = vocab_db.randomAdvanceWord();
 
                     var bubble = Object.assign({}, message.flex2.container.bubble);
-                    bubble.header.contents[0].text= word[0] ;
+                    bubble.header.contents[0].text = word[0];
 
-                    delete bubble.hero ;   // No hero image for now 
+                    delete bubble.hero;   // No hero image for now 
 
                     target = word[1].chi;
-                                            
+
                     explain_array = String(target).split("[");  //  explain_array = String(target).split(/(\[)/g);  -> this returns delimiter as item
 
-                    content_array = [] ;
+                    content_array = [];
 
                     explain_array.forEach(element => {
-                        if (element.length > 0){
-                            item = {} ;
-                            item["type"] = "text" ;
-                            if (element.indexOf("]")>0)
-                                element = "[" + element ;
-                            item["text"] =  element ;
-                            item["size"] = "lg" ;
-                            item["align"] = "start" ;
-                            item["wrap"] =  true ;
-                            content_array.push (item) ;
+                        if (element.length > 0) {
+                            item = {};
+                            item["type"] = "text";
+                            if (element.indexOf("]") > 0)
+                                element = "[" + element;
+                            item["text"] = element;
+                            item["size"] = "lg";
+                            item["align"] = "start";
+                            item["wrap"] = true;
+                            content_array.push(item);
                         }
                     });
                     //bubble.body.contents[0].text = word[1].chi ;
-                    bubble.body.contents = content_array ;
+                    bubble.body.contents = content_array;
 
-                    if ("None" == word[1].link) 
-                        delete bubble.footer ;
-                    else 
-                        bubble.footer.contents[1].action.uri = config.eng.base_url + word[1].link ;
-                    
+                    if ("None" == word[1].link)
+                        delete bubble.footer;
+                    else
+                        bubble.footer.contents[1].action.uri = config.eng.base_url + word[1].link;
+
                     response = Object.assign({}, message.flex2.common);
-                    response.contents = bubble ;
+                    response.contents = bubble;
 
                     //logger.debug(JSON.stringify(response));
                     event.reply(response);
 
-                    break ;
+                    break;
                 }
                 default: {
-                    if (config.ai.enable)  {
+                    if (config.ai.enable) {
                         (async () => {
                             try {
-                            const res = await fetch(config.ai.url + event.source.userId + "?" + new URLSearchParams({
-                                ask: text
-                            }).toString()) ;
-                            
-                            logger.debug (config.ai.url+ event.source.userId + "?" + new URLSearchParams({
-                                ask: text
-                            }).toString())
+                                const res = await fetch(config.ai.url + event.source.userId + "?" + new URLSearchParams({
+                                    ask: text
+                                }).toString());
 
-                            reply = await res.text()
-                            response =  Object.assign({}, message.text); 
-                            response.text =  reply ;
-                            logger.debug(reply)
-                            event.reply(response);
-                            
+                                logger.debug(config.ai.url + event.source.userId + "?" + new URLSearchParams({
+                                    ask: text
+                                }).toString())
+
+                                reply = await res.text()
+                                response = Object.assign({}, message.text);
+                                response.text = reply;
+                                logger.debug(reply)
+                                event.reply(response);
+
                             } catch (err) {
-                            logger.error(err.message); //can be console.error
+                                logger.error(err.message); //can be console.error
                             }
                         })();
                     }
-                    else  {
-                        response =  Object.assign({}, message.text); 
-                        response.text =  event.message.text ;
+                    else {
+                        response = Object.assign({}, message.text);
+                        response.text = event.message.text;
                         event.reply(response);
                     }
 
-                    
+
                     break;
                 }
             }
@@ -202,40 +202,46 @@ bot.on('message', function (event) {
             break;
         }
         case "sticker": {
-            response = Object.assign({},  message.sticker); ;
+            response = Object.assign({}, message.sticker);;
             if (event.message.packageId == "1")
                 response.stickerId = event.message.stickerId;
             event.reply(response);
             break;
         }
-        case "location" :{
-            response.text = event.message.address + " (" + event.message.latitude + "," + event.message.longitude + ")" ;
+        case "location": {
+            response.text = event.message.address + " (" + event.message.latitude + "," + event.message.longitude + ")";
             event.reply(response);
             break
         }
-        case "image":  default : {
-            response.text = config.linebot.service_not_support ;
+        case "image": default: {
+            response.text = config.linebot.service_not_support;
             event.reply(response);
-            break ;
+            break;
         }
     }
 
     //response["quickReply"] = message.quickReply ;
-    log_msg = MEMBERS[event.source.userId]["displayName"] + " 說 " + event.message.text ;
-    logger.info (log_msg) ;
-    if (config.mqtt.enable) 
-        mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg) ;
-    
+    id = event.source.userId;
+    if (MEMBERS[id])
+        id = MEMBERS[id]["displayName"]
+    log_msg = id + " 說 " + event.message.text;
+    logger.info(log_msg);
+    if (config.mqtt.enable)
+        mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg);
+
 });
 
 
 bot.on('leave', function (event) {
     logger.debug(event);
-    log_msg = MEMBERS[event.source.userId]["displayName"] + " 離開麵包國了 !"
-    logger.info (log_msg) ;
-    mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg) ;
+    id = event.source.userId;
+    if (MEMBERS[id])
+        id = MEMBERS[id]["displayName"]
+    log_msg = id + " 說 " + " 離開麵包國了 !";
+    logger.info(log_msg);
+    mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg);
     unregisterMember(event.source.userId);
-    
+
 })
 
 bot.on('join', function (event) {
@@ -244,9 +250,14 @@ bot.on('join', function (event) {
         registerMember(profile);
         var msg = config.linebot.welcome_join_string.replace("$USER", profile.displayName);
         event.reply(msg);
-        log_msg =  MEMBERS[event.source.userId]["displayName"] + " 加入麵包國了 !" ;
-        logger.info (log_msg) ;
-        mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg) ;
+
+        id = event.source.userId;
+        if (MEMBERS[id])
+            id = MEMBERS[id]["displayName"]
+        log_msg = id + " 加入麵包國了 !";
+
+        logger.info(log_msg);
+        mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg);
     });
 })
 
@@ -257,17 +268,26 @@ bot.on('follow', function (event) {
         var msg = config.linebot.welcome_string.replace("$USER", profile.displayName);
         event.reply(msg);
 
-        log_msg =  MEMBERS[event.source.userId]["displayName"] + " 回到麵包國了 !" ;
-        logger.info (log_msg) ;
-        mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg) ;
+        id = event.source.userId;
+        if (MEMBERS[id])
+            id = MEMBERS[id]["displayName"]
+        log_msg = id + " 回到麵包國了 !";
+
+        logger.info(log_msg);
+        mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg);
     });
 })
 
 bot.on('unfollow', function (event) {
     logger.debug(event);
-    log_msg =  MEMBERS[event.source.userId]["displayName"] +  " 封鎖麵包國了 !" ;
-    logger.info (log_msg) ;
-    mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg) ;
+
+    id = event.source.userId;
+    if (MEMBERS[id])
+        id = MEMBERS[id]["displayName"]
+    log_msg = id + " 封鎖麵包國了 !";
+
+    logger.info(log_msg);
+    mqtt_agent.publish(config.mqtt.topic_for_homebot, log_msg);
     unregisterMember(event.source.userId);
 
 
@@ -291,14 +311,14 @@ function registerMember(profile) {
 
 function unregisterMember(userId) {
     delete MEMBERS[userId];
-    console.log (Object.keys(MEMBERS).length) ;
+    console.log(Object.keys(MEMBERS).length);
     logger.debug(JSON.stringify(MEMBERS));
 }
 
 setTimeout(function () {
     var sendMsg = config.linebot.service_is_up_string;
     if (Object.keys(MEMBERS).length > 0) {
-        bot.broadcast( sendMsg);
+        bot.broadcast(sendMsg);
         logger.info('send: ' + Object.keys(MEMBERS) + ':' + sendMsg);
     }
     else {
@@ -310,9 +330,9 @@ setTimeout(function () {
 /****************************************************************************
  *    Main Start
  ****************************************************************************/
-MEMBERS = member_manager.loadMemberProfiles() 
+MEMBERS = member_manager.loadMemberProfiles()
 
-vocab_db.init ();
+vocab_db.init();
 /*
 console.log (vocab_db.randomBasicWord()) ;
 console.log (vocab_db.randomAdvanceWord()) ;
